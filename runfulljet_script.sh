2652e86bac3a747abd76e5ebb0e98e5b3391195c
Archita-Dash <archita.rani.dash@cern.ch>
2023-05-23 01:52:40 +0200

    basic task for full jet reconstruction. This still doesn't seem to work yet. ERROR] SEVERE: Device internal-dpl-aod-reader (31157) had at least one message above severity 4: Exception caught: Couldn't get TTree DF_2247591108861852/O2emcalclusters from AO2D.root. Please check https://aliceo2group.github.io/analysis-framework/docs/troubleshooting/#tree-not-found for more information.

---
+++ runfulljet_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FileIn="$1"
+JSON="$2"
+o2-analysis-timestamp -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-trackselection -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-track-propagation -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-event-selection -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-zdc-converter -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-je-jet-finder -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-je-jet-full-dilepton-correlation -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error
