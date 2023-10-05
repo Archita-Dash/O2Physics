77ac5d225b9de726cc0ded5fb1a637019d8c8725
Archita-Dash <archita.rani.dash@cern.ch>
2023-10-05 14:18:47 +0200

    cleaning up the code a bit

--- runfulljet_script.sh
+++ runfulljet_script.sh
@@ -17,4 +17,4 @@ o2-analysis-je-emcal-correction-task -b --configuration json://$JSON --aod-memor
 o2-analysis-pid-tpc-full -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
 o2-analysis-event-selection -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
 o2-analysis-pid-tpc-base -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error 
-#o2-analysis-je-jet-full-dilepton-correlation -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error
+
